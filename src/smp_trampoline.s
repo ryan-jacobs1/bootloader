e8f0a2165105c72ce9c5f360fc89bda287930317
Ryan Jacobs <ryanjacobs@utexas.edu>
2020-08-10 23:47:33 -0500

    changes to smp trampoline

--- src/smp_trampoline.s
+++ src/smp_trampoline.s
@@ -5,4 +5,27 @@
 .code16
 
 _smp_trampoline:
-    jmp _smp_trampoline
+    cli
+
+trampoline_gdt:
+    
+    .quad 0
+
+; __KERNEL_CS
+	.word 0xFFFF
+	.word 0
+	.byte 0
+	.byte 0b10011010
+	.byte 0b11001111
+	.byte 0
+
+	; __KNLUSR_DS
+	.word 0xFFFF
+	.word 0
+	.byte 0
+	.byte 0b10010010
+	.byte 0b11001111
+	.byte 0
+trampoline_gdtDesc:
+	.word (trampoline_gdtDesc - trampoline_gdt) - 1
+	.quad trampoline_gdt
